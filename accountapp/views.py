51cd2585e100e0d8bbeafd3613c13a05bd1967bf
Kogoon <gojs712@gmail.com>
2021-12-20 22:03:36 +0900

    [Study] Get DB data, for in jinja, HttpResponseRedirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST Method!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     elif request.method == "GET":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET Method!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
