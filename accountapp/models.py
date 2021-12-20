2a952494e8db0762bd1c30eb623fd088389dca81
Kogoon <gojs712@gmail.com>
2021-12-20 21:05:38 +0900

    [Test] Model, DB

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+    # python manage.py makemigrateions -> Create model HelloWorld
+    # python manage.py migrate -> Apply
