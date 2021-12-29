7d4f1a8acfd969b27398b5f0d06916b03c6a3026
Kogoon <gojs712@gmail.com>
2021-12-29 17:13:12 +0900

    [Update] develope & deploy setting

---
+++ koterest/settings/deploy.py
@@ -0,0 +1,36 @@
+from .base import *
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+# Django environment
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+# 배포 시에는 신경 써야 하는 설정
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'password',
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
